90c6e4aa6292d6587737736a11ac6702708216ae
rileyge <grlsr@163.com>
2021-12-17 11:12:05 +0800

    智能合约相关注释完成

--- PlatONet/PlatONContract.cs
+++ PlatONet/PlatONContract.cs
@@ -6,53 +6,107 @@ using System.Threading.Tasks;
 
 namespace PlatONet
 {
+    /// <summary>
+    /// PlatON网络的智能合约
+    /// </summary>
     public class PlatONContract
     {
-        //public Account Account { get; set; }
-        public Contract EthContract
+        internal Contract EthContract
         {
             get
             {
                 return _contract;
             }
         }
+        /// <summary>
+        /// 与该智能合约相关联的PlatON网络信息
+        /// </summary>
         public PlatON PlatON { get; private set; }
+        /// <summary>
+        /// 智能合约地址
+        /// </summary>
         public Address Address { get; private set; }
         private Contract _contract;
+        /// <summary>
+        /// 初始化<see cref="PlatONContract"/>实例
+        /// </summary>
+        /// <param name="client">JsonRpc客户端</param>
+        /// <param name="abi">智能合约的abi</param>
+        /// <param name="address">智能合约地址</param>
+        /// <param name="platon">与智能合约相关联的PlatON网络信息</param>
         public PlatONContract(IClient client, string abi, string address, PlatON platon) :
             this(client, abi, new Address(address), platon)
         { }
+        /// <summary>
+        /// 初始化<see cref="PlatONContract"/>实例
+        /// </summary>
+        /// <param name="client">JsonRpc客户端</param>
+        /// <param name="abi">智能合约的abi</param>
+        /// <param name="address">智能合约地址</param>
+        /// <param name="platon">与智能合约相关联的PlatON网络信息</param>
         public PlatONContract(IClient client, string abi, Address address, PlatON platon)            
         {
             PlatON = platon;
             Address = address;
             _contract = new Contract(new Nethereum.RPC.EthApiService(client), abi, address.ToEthereumAddress());
         }
+        /// <summary>
+        /// 实例化与当前智能合约的<see cref="PlatONFunction"/>对象
+        /// </summary>
+        /// <param name="functionName">函数名</param>
+        /// <returns><see cref="PlatONFunction"/>实例</returns>
         public PlatONFunction GetFunction(string functionName)
         {
             return new PlatONFunction(this, _contract.ContractBuilder.GetFunctionBuilder(functionName));
         }
     }
+    /// <summary>
+    /// 智能合约的函数
+    /// </summary>
     public class PlatONFunction
     {
         //public Account Account { get; set; }
         private Function _function;
         private PlatONContract _contract;
+        /// <summary>
+        /// 实例化<see cref="PlatONFunction"/>实例<br/>
+        /// 注：该方法一般不直接使用，一般使用<see cref="PlatONContract.GetFunction(string)"/>函数来实例化<see cref="PlatONFunction"/>实例。
+        /// </summary>
+        /// <param name="contract">智能合约</param>
+        /// <param name="functionBuilder">`FunctionBuilder`对象</param>
         public PlatONFunction(PlatONContract contract, FunctionBuilder functionBuilder)            
         {
             _contract = contract;
             _function = new Function(contract.EthContract, functionBuilder);
         }
+        /// <summary>
+        /// 估算当前方法执行所需要的Gas
+        /// </summary>
+        /// <param name="functionInput">当前方法执行所需要的参数</param>
+        /// <returns>当前方法执行所需要的Gas</returns>
         public HexBigInteger EstimateGas(params object[] functionInput)
         {
             var result = EstimateGasAsync(functionInput: functionInput);
             result.Wait();
             return result.Result;
         }
+        /// <summary>
+        /// 估算当前方法执行所需要的Gas--异步方法
+        /// </summary>
+        /// <param name="functionInput">当前方法执行所需要的参数</param>
+        /// <returns>当前方法执行所需要的Gas</returns>
         public Task<HexBigInteger> EstimateGasAsync(params object[] functionInput)
         {
             return _function.EstimateGasAsync(functionInput);
         }
+        /// <summary>
+        /// 估算当前方法执行所需要的Gas
+        /// </summary>
+        /// <param name="from">当前方法的发送人地址</param>
+        /// <param name="gas">预设的Gas数量</param>
+        /// <param name="value">随方法一起发送的lat/atp数量</param>
+        /// <param name="functionInput">当前方法执行所需要的参数</param>
+        /// <returns>当前方法执行所需要的Gas</returns>
         public HexBigInteger EstimateGas(string from, HexBigInteger gas,
             HexBigInteger value, params object[] functionInput)
         {
@@ -60,6 +114,14 @@ namespace PlatONet
             result.Wait();
             return result.Result;
         }
+        /// <summary>
+        /// 估算当前方法执行所需要的Gas--异步方法
+        /// </summary>
+        /// <param name="from">当前方法的发送人地址</param>
+        /// <param name="gas">预设的Gas数量</param>
+        /// <param name="value">随方法一起发送的lat/atp数量</param>
+        /// <param name="functionInput">当前方法执行所需要的参数</param>
+        /// <returns>当前方法执行所需要的Gas</returns>
         public Task<HexBigInteger> EstimateGasAsync(string from, HexBigInteger gas,
             HexBigInteger value, params object[] functionInput)
         {
@@ -72,12 +134,33 @@ namespace PlatONet
                 etherAddress = (new Address(from)).ToEthereumAddress();
             return _function.EstimateGasAsync(etherAddress, gas, value, functionInput);
         }
+        /// <summary>
+        /// 发送交易<br/>
+        /// 注：1. PlatON中的<see cref="SendTransaction(object[])"/>方法与Web3.js不同，
+        /// 详情请参照<see cref="PlatON.SendTransaction(Transaction)"/>中的说明。<br/>
+        /// 2. 未指定的参数PlatON会自动查询网络默认值。
+        /// </summary>
+        /// <param name="functionInput">当前方法执行所需要的参数</param>
+        /// <returns>交易hash</returns>
         public string SendTransaction(params object[] functionInput)
         {
             var result = SendTransactionAsync(functionInput: functionInput);
             result.Wait();
             return result.Result;
         }
+        /// <summary>
+        /// 发送交易<br/>
+        /// 注：1. PlatON中的<see cref="SendTransaction(object[])"/>方法与Web3.js不同，
+        /// 详情请参照<see cref="PlatON.SendTransaction(Transaction)"/>中的说明。<br/>
+        /// 2. 未指定的参数PlatON会自动查询网络默认值。
+        /// </summary>
+        /// <param name="from">当前方法的发送人地址</param>
+        /// <param name="gas">预设的Gas数量</param>
+        /// <param name="gasPrice">预设的GasPrice</param>
+        /// <param name="value">随方法一起发送的lat/atp数量</param>
+        /// <param name="nonce">nonce值</param>
+        /// <param name="functionInput">当前方法执行所需要的参数</param>
+        /// <returns>交易hash</returns>
         public string SendTransaction(string from, HexBigInteger gas, HexBigInteger gasPrice,
             HexBigInteger value, HexBigInteger nonce, params object[] functionInput)
         {
@@ -85,10 +168,31 @@ namespace PlatONet
             result.Wait();
             return result.Result;
         }
+        /// <summary>
+        /// 发送交易--异步方法<br/>
+        /// 注：PlatON中的<see cref="SendTransactionAsync(object[])"/>方法与Web3.js不同，
+        /// 详情请参照<see cref="PlatON.SendTransactionAsync(Transaction)"/>中的说明。<br/>
+        /// 2. 未指定的参数PlatON会自动查询网络默认值。
+        /// </summary>
+        /// <param name="functionInput">当前方法执行所需要的参数</param>
+        /// <returns>交易hash</returns>
         public Task<string> SendTransactionAsync(params object[] functionInput)
         {
             return SendTransactionAsync(null, null, null, null, null, functionInput: functionInput);
         }
+        /// <summary>
+        /// 发送交易--异步方法<br/>
+        /// 注：PlatON中的<see cref="SendTransactionAsync(object[])"/>方法与Web3.js不同，
+        /// 详情请参照<see cref="PlatON.SendTransactionAsync(Transaction)"/>中的说明。<br/>
+        /// 2. 未指定的参数PlatON会自动查询网络默认值。
+        /// </summary>
+        /// <param name="from">当前方法的发送人地址</param>
+        /// <param name="gas">预设的Gas数量</param>
+        /// <param name="gasPrice">预设的GasPrice</param>
+        /// <param name="value">随方法一起发送的lat/atp数量</param>
+        /// <param name="nonce">nonce值</param>
+        /// <param name="functionInput">当前方法执行所需要的参数</param>
+        /// <returns>交易hash</returns>
         public Task<string> SendTransactionAsync(string from, HexBigInteger gas, HexBigInteger gasPrice,
             HexBigInteger value, HexBigInteger nonce, params object[] functionInput)
         {            
@@ -118,12 +222,29 @@ namespace PlatONet
             tx.Sign(_contract?.PlatON?.Account);
             return _contract.PlatON.SendRawTransactionAsync(tx.SignedTransaction.ToHex());
         }
+        /// <summary>
+        /// 消息调用<br/>
+        /// 消息调用交易直接在节点旳VM中执行而 不需要通过区块链的挖矿来执行。
+        /// </summary>
+        /// <typeparam name="TReturn">返回数据的数据类型</typeparam>
+        /// <param name="functionInput">当前方法执行所需要的参数</param>
+        /// <returns>查询返回的数据</returns>
         public TReturn Call<TReturn>(params object[] functionInput)
         {
             var result = CallAsync<TReturn>(functionInput: functionInput);
             result.Wait();
             return result.Result;
         }
+        /// <summary>
+        /// 消息调用<br/>
+        /// 消息调用交易直接在节点旳VM中执行而 不需要通过区块链的挖矿来执行。
+        /// </summary>
+        /// <typeparam name="TReturn">返回数据的数据类型</typeparam>
+        /// <param name="from">当前方法的发送人地址</param>
+        /// <param name="gas">预设的Gas数量</param>
+        /// <param name="value">随方法一起发送的lat/atp数量。</param>
+        /// <param name="functionInput">当前方法执行所需要的参数</param>
+        /// <returns>查询返回的数据</returns>
         public TReturn Call<TReturn>(string from, HexBigInteger gas,
             HexBigInteger value, params object[] functionInput)
         {
@@ -131,6 +252,17 @@ namespace PlatONet
             result.Wait();
             return result.Result;
         }
+        /// <summary>
+        /// 消息调用<br/>
+        /// 消息调用交易直接在节点旳VM中执行而 不需要通过区块链的挖矿来执行。
+        /// </summary>
+        /// <typeparam name="TReturn">返回数据的数据类型</typeparam>
+        /// <param name="from">当前方法的发送人地址</param>
+        /// <param name="gas">预设的Gas数量</param>
+        /// <param name="value">随方法一起发送的lat/atp数量。</param>
+        /// <param name="block"><see cref="BlockParameter"/>实例，表示查询截止块高</param>
+        /// <param name="functionInput">当前方法执行所需要的参数</param>
+        /// <returns>查询返回的数据</returns>
         public TReturn Call<TReturn>(string from, HexBigInteger gas,
             HexBigInteger value, BlockParameter block, params object[] functionInput)
         {
@@ -138,33 +270,75 @@ namespace PlatONet
             result.Wait();
             return result.Result;
         }
+        /// <summary>
+        /// 消息调用<br/>
+        /// 消息调用交易直接在节点旳VM中执行而 不需要通过区块链的挖矿来执行。
+        /// </summary>
+        /// <typeparam name="TReturn">返回数据的数据类型</typeparam>
+        /// <param name="block"><see cref="BlockParameter"/>实例，表示查询截止块高</param>
+        /// <param name="functionInput">当前方法执行所需要的参数</param>
+        /// <returns>查询返回的数据</returns>
         public TReturn Call<TReturn>(BlockParameter block, params object[] functionInput)
         {
             var result = CallAsync<TReturn>(block, functionInput: functionInput);
             result.Wait();
             return result.Result;
         }
+        /// <summary>
+        /// 消息调用--异步操作<br/>
+        /// 消息调用交易直接在节点旳VM中执行而 不需要通过区块链的挖矿来执行。
+        /// </summary>
+        /// <typeparam name="TReturn">返回数据的数据类型</typeparam>
+        /// <param name="functionInput">当前方法执行所需要的参数</param>
+        /// <returns>查询返回的数据</returns>
         public Task<TReturn> CallAsync<TReturn>(params object[] functionInput)
         {
             return _function.CallAsync<TReturn>(functionInput);
         }
+        /// <summary>
+        /// 消息调用--异步操作<br/>
+        /// 消息调用交易直接在节点旳VM中执行而 不需要通过区块链的挖矿来执行。
+        /// </summary>
+        /// <typeparam name="TReturn">返回数据的数据类型</typeparam>
+        /// <param name="from">当前方法的发送人地址</param>
+        /// <param name="gas">预设的Gas数量</param>
+        /// <param name="value">随方法一起发送的lat/atp数量。</param>
+        /// <param name="functionInput">当前方法执行所需要的参数</param>
+        /// <returns>查询返回的数据</returns>
         public Task<TReturn> CallAsync<TReturn>(string from, HexBigInteger gas,
             HexBigInteger value, params object[] functionInput)
         {
             var sender = new Address(from);
             return _function.CallAsync<TReturn>(sender.ToEthereumAddress(), gas, value, functionInput);
         }
+        /// <summary>
+        /// 消息调用--异步操作<br/>
+        /// 消息调用交易直接在节点旳VM中执行而 不需要通过区块链的挖矿来执行。
+        /// </summary>
+        /// <typeparam name="TReturn">返回数据的数据类型</typeparam>
+        /// <param name="from">当前方法的发送人地址</param>
+        /// <param name="gas">预设的Gas数量</param>
+        /// <param name="value">随方法一起发送的lat/atp数量。</param>
+        /// <param name="block"><see cref="BlockParameter"/>实例，表示查询截止块高</param>
+        /// <param name="functionInput">当前方法执行所需要的参数</param>
+        /// <returns>查询返回的数据</returns>
         public Task<TReturn> CallAsync<TReturn>(string from, HexBigInteger gas,
             HexBigInteger value, BlockParameter block, params object[] functionInput)
         {
             var sender = new Address(from);
             return _function.CallAsync<TReturn>(sender.ToEthereumAddress(), gas, value, block, functionInput);
         }
+        /// <summary>
+        /// 消息调用--异步操作<br/>
+        /// 消息调用交易直接在节点旳VM中执行而 不需要通过区块链的挖矿来执行。
+        /// </summary>
+        /// <typeparam name="TReturn">返回数据的数据类型</typeparam>
+        /// <param name="block"><see cref="BlockParameter"/>实例，表示查询截止块高</param>
+        /// <param name="functionInput">当前方法执行所需要的参数</param>
+        /// <returns>查询返回的数据</returns>
         public Task<TReturn> CallAsync<TReturn>(BlockParameter block, params object[] functionInput)
         {
             return _function.CallAsync<TReturn>(block, functionInput);
         }
-
-
     }
 }
